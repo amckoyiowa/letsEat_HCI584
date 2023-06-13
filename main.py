4f80cbff6052b381327e9f393e33fcaff36331a3
Ashley Mckoy <ashleymckoy@ashleys-air.mynetworksettings.com>
2023-06-13 17:09:09 -0400

    Adding imports

---
+++ main.py
@@ -0,0 +1,13 @@
+#Beginning imports, may change later
+from tkinter import *
+import tkinter.filedialog 
+import tkinter.messagebox
+import feedparser
+import glob
+import os
+import urllib.parse # for making a safe URL
+import requests 
+import random
+import string
+from pprint import pprint
+
